5272bae0ef14d78c2a38a6562d340259052851df
jorgecarloslopez <lopez.cs.jorge@gmail.com>
2024-11-03 22:27:43 -0300

    Select all image elements inside the gallery I am declaring a variable with a fixed data using the const word

---
+++ index.js
@@ -0,0 +1,5 @@
+const galleryImages = document.querySelectorAll(".gallery img");
+const modal = document.getElementById("modal");
+const modalImage = document.getElementById("modal-image");
+const caption = document.getElementById("caption");
+const closeButton = document.getElementById("close");
