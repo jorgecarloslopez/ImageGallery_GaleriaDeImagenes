aaa718e1b8fd69f204abbd06f1c0f2c43e751d51
jorgecarloslopez <lopez.cs.jorge@gmail.com>
2024-11-04 23:32:02 -0300

    Add modal functionality for image gallery with JavaScript

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const modal = document.getElementById("modal");
 const modalImage = document.getElementById("modal-image");
 const caption = document.getElementById("caption");
 const closeButton = document.getElementById("close");
+
+galleryImages.forEach((img) => {
+  img.addEventListener("click", () => {
+    modal.style.display = "block";
+    modalImage.src = img.src;
+    caption.innerText = img.alt;
+  });
+});
